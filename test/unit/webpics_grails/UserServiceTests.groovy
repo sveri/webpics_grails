74755d98cf76b56f439efe088368f820d38a0953
sveri <sveri@sveri.de>
2013-01-09 23:10:30 +0100

    added fixtures and tests

--- test/unit/webpics_grails/UserServiceTests.groovy
+++ test/unit/webpics_grails/UserServiceTests.groovy
@@ -4,6 +4,7 @@ package webpics_grails
 
 import grails.test.mixin.*
 import org.junit.*
+import webpics_grails.auth.User
 
 /**
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
@@ -11,7 +12,16 @@ import org.junit.*
 @TestFor(UserService)
 class UserServiceTests {
 
-    void testAdminShouldBeAllowedToSeeAllAlbums() {
-        def admin = new User()
+    def fixtureLoader
+
+    void setUp(){
+        fixtureLoader.load("users")
     }
+
+    void testAdminShouldBeAllowedToSeeAllAlbums() {
+//        def admin = new User(username: "Admin", roles: "Administrator", passwordHash: "aslkfd", passwordSalt: "alskfj").save()
+//        service.isUserAllowedToSeeAlbum()
+        assert 3 == 3
+    }
+
 }
