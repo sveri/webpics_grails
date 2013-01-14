1170c64e4122f42085213dea5ebab8857ca86fec
Sven Richter <webpics@sveri.de>
2013-01-14 22:30:21 +0100

    removed unimplemented tests

--- test/unit/webpics_grails/controller/AlbumControllerTests.groovy
+++ test/unit/webpics_grails/controller/AlbumControllerTests.groovy
@@ -5,15 +5,11 @@ package webpics_grails.controller
 import grails.test.mixin.*
 import org.junit.*
 
-import webpics_grails.AlbumController;
+import webpics_grails.AlbumController
 
 /**
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
 @TestFor(AlbumController)
 class AlbumControllerTests {
-
-    void testSomething() {
-       fail "Implement me"
-    }
 }
--- test/unit/webpics_grails/controller/RoleControllerTests.groovy
+++ test/unit/webpics_grails/controller/RoleControllerTests.groovy
@@ -5,15 +5,11 @@ package webpics_grails.controller
 import grails.test.mixin.*
 import org.junit.*
 
-import webpics_grails.auth.RoleController;
+import webpics_grails.auth.RoleController
 
 /**
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
 @TestFor(RoleController)
 class RoleControllerTests {
-
-    void testSomething() {
-       fail "Implement me"
-    }
 }
